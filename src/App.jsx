d899704dc9976a495438ed6c84ad02279392741b
Youngking Mike <youngking4406@gmail.com>
2024-01-18 08:04:56 +0000

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
-function App() {
-  return <h1>Hello World</h1>;
-}
+const App = () => {
+  return (
+    <Router>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
