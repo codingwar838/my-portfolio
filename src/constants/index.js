1382c9330d33b1041ee6d1255b74158157199574
Youngking Mike <youngking4406@gmail.com>
2024-01-20 09:45:53 +0000

    Updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -120,7 +120,7 @@ const experiences = [
     company_name: "Starbucks",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "Feb 2020 - Jan 2021",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -129,7 +129,7 @@ const experiences = [
     ],
   },
   {
-    title: "React Native Developer",
+    title: "React & Node.js Developer",
     company_name: "Tesla",
     icon: tesla,
     iconBg: "#E6DEDD",
